0e6b67dda06a818d516cfa2f570b130ab5a17f2a
dwijanto11 <dwijanto10@gmail.com>
2022-06-19 14:08:51 +0700

    menambahkan jawaban

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,9 +2,16 @@
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
 
-const Contact = () => {
+const Contact = ({data}) => {
     return (
-        <></>
+        <div className='contacts'>
+            <img className='contacts_image' src={data.photo} alt={data.name} />
+            <div className='contacts_content'>
+                <h2 className='contacts_title'>{data.name}</h2>
+                <h5 className='contacts_phone'>{data.phone}</h5>
+                <p className='contacts_email'>{data.email}</p>
+            </div>
+        </div>
     )
 }
 
